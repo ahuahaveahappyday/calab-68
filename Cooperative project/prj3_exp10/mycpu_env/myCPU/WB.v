215c232b8108c510bb5147dc3c268e2c9f95ed8c
wang jinghua <155536198+ahuahaveahappyday@users.noreply.github.com>
2024-09-25 12:00:44 +0800

    Add files via upload

---
+++ Cooperative project/prj3_exp10/mycpu_env/myCPU/WB.v
@@ -0,0 +1,57 @@
+module WBreg(
+    input  wire        clk,
+    input  wire        resetn,
+
+    output wire        ws_allowin,
+    input  wire [37:0] ms_rf_zip, // {ms_rf_we, ms_rf_waddr, ms_rf_wdata}
+    input  wire        ms_to_ws_valid,
+    input  wire [31:0] ms_pc,    
+
+    output wire [31:0] debug_wb_pc,
+    output wire [ 3:0] debug_wb_rf_we,
+    output wire [ 4:0] debug_wb_rf_wnum,
+    output wire [31:0] debug_wb_rf_wdata,
+
+
+    output wire [37:0] ws_rf_zip  // {ws_rf_we, ws_rf_waddr, ws_rf_wdata}
+);
+    
+    wire        ws_ready_go;
+    reg         ws_valid;
+    reg  [31:0] ws_pc;
+    reg  [31:0] ws_rf_wdata;
+    reg  [4 :0] ws_rf_waddr;
+    reg         ws_rf_we;
+
+
+
+    assign ws_ready_go      = 1'b1;
+    assign ws_allowin       = ~ws_valid | ws_ready_go ;     
+    always @(posedge clk) begin
+        if(~resetn)
+            ws_valid <= 1'b0;
+        else if(ws_allowin)
+            ws_valid <= ms_to_ws_valid; 
+    end
+
+
+    always @(posedge clk) begin
+        if(~resetn) begin
+            ws_pc <= 32'b0;
+            {ws_rf_we, ws_rf_waddr, ws_rf_wdata} <= 38'b0;
+        end
+        if(ms_to_ws_valid & ws_allowin) begin
+            ws_pc <= ms_pc;
+            {ws_rf_we, ws_rf_waddr, ws_rf_wdata} <= ms_rf_zip;
+        end
+    end
+
+
+    assign ws_rf_zip = {ws_rf_we & ws_valid, ws_rf_waddr, ws_rf_wdata};
+
+
+    assign debug_wb_pc = ws_pc;
+    assign debug_wb_rf_wdata = ws_rf_wdata;
+    assign debug_wb_rf_we = {4{ws_rf_we & ws_valid}};
+    assign debug_wb_rf_wnum = ws_rf_waddr;
+endmodule
---
+++ Cooperative project/prj3_exp10/mycpu_env/myCPU/cpu.v
@@ -0,0 +1,131 @@
+module mycpu_top(
+    input  wire        clk,
+    input  wire        resetn,
+//cpu与指令存储器交互的接口
+    output wire        inst_sram_en,
+    output wire [ 3:0] inst_sram_we,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata,
+    input  wire [31:0] inst_sram_rdata,
+//cpu与数据存储器交互的接口
+    output wire        data_sram_en,
+    output wire [ 3:0] data_sram_we,
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata,
+    input  wire [31:0] data_sram_rdata,
+//debug信号
+    output wire [31:0] debug_wb_pc,
+    output wire [ 3:0] debug_wb_rf_we,
+    output wire [ 4:0] debug_wb_rf_wnum,
+    output wire [31:0] debug_wb_rf_wdata
+);
+    wire        id_allowin;
+    wire        ex_allowin;
+    wire        mem_allowin;
+    wire        wb_allowin;
+
+    wire        if_to_id_valid;
+    wire        id_to_ex_valid;
+    wire        ex_to_mem_valid;
+    wire        mem_to_wb_valid;
+
+    wire 
+    wire [31:0] es_pc;
+    wire [31:0] ms_pc;
+
+    wire [38:0] es_rf_zip;
+    wire [37:0] ms_rf_zip;
+    wire [37:0] ws_rf_zip;
+
+    wire [32:0] br_zip;
+    wire [64 -1:0] if_to_id_bus;
+    wire [148 -1:0] id_to_ex_bus;
+
+
+    IFreg my_ifReg(
+        .clk(clk),
+        .resetn(resetn),
+
+        .inst_sram_en(inst_sram_en),
+        .inst_sram_we(inst_sram_we),
+        .inst_sram_addr(inst_sram_addr),
+        .inst_sram_wdata(inst_sram_wdata),
+        .inst_sram_rdata(inst_sram_rdata),
+        
+        .ds_allowin(ds_allowin),
+        .br_zip(br_zip),
+        .fs_to_ds_valid(fs_to_ds_valid),
+        .fs_to_ds_bus(fs_to_ds_bus)
+    );
+
+    IDreg my_idReg(
+        .clk(clk),
+        .resetn(resetn),
+
+        .ds_allowin(ds_allowin),
+        .br_zip(br_zip),
+        .fs_to_ds_valid(fs_to_ds_valid),
+        .fs_to_ds_bus(fs_to_ds_bus),
+
+        .es_allowin(es_allowin),
+        .ds_to_es_valid(ds_to_es_valid),
+        .ds_to_es_bus(ds_to_es_bus),
+
+        .ws_rf_zip(ws_rf_zip),
+        .ms_rf_zip(ms_rf_zip),
+        .es_rf_zip(es_rf_zip)
+    );
+
+    EXEreg my_exeReg(
+        .clk(clk),
+        .resetn(resetn),
+        
+        .es_allowin(es_allowin),
+        .ds_to_es_valid(ds_to_es_valid),
+        .ds_to_es_bus(ds_to_es_bus),
+
+        .ms_allowin(ms_allowin),
+        .es_rf_zip(es_rf_zip),
+        .es_to_ms_valid(es_to_ms_valid),
+        .es_pc(es_pc),
+        
+        .data_sram_en(data_sram_en),
+        .data_sram_we(data_sram_we),
+        .data_sram_addr(data_sram_addr),
+        .data_sram_wdata(data_sram_wdata)
+    );
+
+    MEMreg my_memReg(
+        .clk(clk),
+        .resetn(resetn),
+
+        .ms_allowin(ms_allowin),
+        .es_rf_zip(es_rf_zip),
+        .es_to_ms_valid(es_to_ms_valid),
+        .es_pc(es_pc),
+
+        .ws_allowin(ws_allowin),
+        .ms_rf_zip(ms_rf_zip),
+        .ms_to_ws_valid(ms_to_ws_valid),
+        .ms_pc(ms_pc),
+
+        .data_sram_rdata(data_sram_rdata)
+    ) ;
+
+    WBreg my_wbReg(
+        .clk(clk),
+        .resetn(resetn),
+
+        .ws_allowin(ws_allowin),
+        .ms_rf_zip(ms_rf_zip),
+        .ms_to_ws_valid(ms_to_ws_valid),
+        .ms_pc(ms_pc),
+
+        .debug_wb_pc(debug_wb_pc),
+        .debug_wb_rf_we(debug_wb_rf_we),
+        .debug_wb_rf_wnum(debug_wb_rf_wnum),
+        .debug_wb_rf_wdata(debug_wb_rf_wdata),
+
+        .ws_rf_zip(ws_rf_zip)
+    );
+endmodule
