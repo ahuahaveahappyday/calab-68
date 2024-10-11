ecb2fb9a730036dfea304016b60b2323f5c0b0a5
aihuachun <zhengfengyutian@gmail.com>
2024-10-11 11:12:40 +0800

    feat(csrfile.v): done crmd_plv

--- Cooperative_project/prj4_exp12/mycpu_env/myCPU/csrfile.v
+++ Cooperative_project/prj4_exp12/mycpu_env/myCPU/csrfile.v
@@ -1,18 +1,66 @@
+`define CSR_CRMD 0
+
+
+
+
 module csrfile(
     input  wire        clk,
-    // inst port
+    input  wire        reset,
+    // inst access-------------------------
+    // read port
     input wire          csr_re,         // read_enable
     input wire  [4:0]   csr_num,        // num of csr, address
     output wire [31:0]  csr_rvalue,
-
+    // write port
     input wire          csr_we,
     input wire  [31:0]  csr_wmask,
-    input wire  [31:0]  csr_wvalue
+    input wire  [31:0]  csr_wvalue,
 
-    // hardware port
+    // hardware access------------------------
+    // exception from wb
+    input wire          wb_ex,
+    input wire          wb_ecode,
+    input wire          wb_esubcode,
+    // inst (ertn) from wb
+    input wire          ertn_flush
     //TODO
 
 );
+reg [1:0]   csr_crmd_plv;
+
+reg [1:0]   csr_prmd_pplv;
+
+
+
+
+
+
+
+/*---------------------------CSMD---------------------------------------------------*/
+// PLV
+always @(posedge clk)begin
+    if(reset)
+        csr_crmd_plv <=     2'b0;
+    else if (wb_ex)             // exception 
+        csr_crmd_plv <=     2'b0;
+    else if(ertn_flush)           // restore to pplv
+        csr_crmd_plv <=     csr_prmd_pplv;
+    else if(csr_we && csr_num == `CSR_CRMD)     // inst access
+        csr_crmd_plv <=     csr_wmask & csr_wvalue
+                            | ~csr_wmask & csr_wvalue;
+end
+
+
+
+
+
+
+
+
+
+
+
+
 //TODO
 
 endmodule
