85be43dd3da2936ef69e1b36a07240eda1544d09
aihuachun <zhengfengyutian@gmail.com>
2024-10-26 21:13:03 +0800

    feat: add part of ar state macine

--- Cooperative_project/prj5_exp15/mycpu_env/myCPU/sram_axi_bridge.v
+++ Cooperative_project/prj5_exp15/mycpu_env/myCPU/sram_axi_bridge.v
@@ -1,4 +1,6 @@
 module sram_axi_bridge(
+    input wire          clk,
+    input wire          resetn,
     //if模块与指令存储器的交互接口
     input wire         inst_sram_req,
     input wire         inst_sram_wr,
@@ -67,10 +69,72 @@ module sram_axi_bridge(
 );
 
 
+/*--------------------------------------read request chanel---------------------------------------------------*/
+assign arlen =      8'b0;
+assign arburst =    2'b01;
+assign arlock =     2'b0;
+assign arache =     4'b0;
+assign arprot =     3'b0;
+
+reg [3:0]       arid_reg;
+reg [31:0]      araddr_reg;
+reg [2:0]       arsize_reg;
+reg             arvalid_reg;
 
 
+reg             ar_data_addr_valid;
+//  ----------------one hot encoding
+localparam      AR_WAIT =       3'b001,
+                AR_INST_SEND=   3'b010,
+                AR_DATA_SEND =  3'b100;
 
+reg [2:0]   ar_current_state;
+reg [2:0]   ar_next_state;
+always @(posedge clk)begin
+    if(~resetn)
+        ar_current_state <= AR_WAIT;
+    else 
+        ar_current_state <= ar_next_state;
+end
+// ------------------next_state generate
+always @( * )begin
+    case (ar_current_state)
+        AR_WAIT:begin
+            if( inst_sram_req & ~inst_sram_wr)      // inst fetch, Higher priority
+                ar_next_state   =       AR_INST_SEND;
+            else if(data_sram_req & ~data_sram_wr)// data fetch
+                ar_next_state =         AR_DATA_SEND;
+            else 
+                ar_next_state =         AR_WAIT;
+        end
+        AR_INST_SEND:begin
+            if  (arready & ar_data_addr_valid)
+                ar_next_state = AR_DATA_SEND;
+            else if(arready)
+                ar_next_state = AR_WAIT;
+            else 
+                ar_next_state = AR_WAIT;
+        end
+        AR_DATA_SEND: begin
+            if  (arready)
+                ar_next_state = AR_WAIT;
+            else 
+                ar_next_state = AR_DATA_SEND;
+        end
+    endcase
+end
 
+//---------------------------sram_like slave 
+assign inst_sram_addr_ok = ar_current_state == AR_WAIT;
+assign data_sram_addr_ok = ar_current_state == AR_WAIT;
+// --------------------------axi master
+assign arid = {2'b0,{ar_current_state == AR_DATA_SEND}};
+always @(posedge clk)begin
+    if(~resetn)
+        araddr_reg <= 32'b0;
+    else if(ar_current_state == AR_WAIT && inst_sram_req & ~inst_sram_wr)
+        araddr_reg <= inst_sram_addr;
+end
 
 
 
