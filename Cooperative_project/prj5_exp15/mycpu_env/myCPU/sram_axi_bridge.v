22243c8ec71dcbe1df8e7de457b8e2eaad469fc1
aihuachun <zhengfengyutian@gmail.com>
2024-10-27 10:19:57 +0800

    feat(sram_axi_bridge): add read request and respond chanel

--- Cooperative_project/prj5_exp15/mycpu_env/myCPU/sram_axi_bridge.v
+++ Cooperative_project/prj5_exp15/mycpu_env/myCPU/sram_axi_bridge.v
@@ -28,19 +28,19 @@ module sram_axi_bridge(
     // read request
     output wire  [3:0]          arid      ,
     output wire  [31:0]         araddr    ,
-    output wire  [7:0]          arlen     ,
-    output wire  [2:0]          arsize    ,
-    output wire  [1:0]          arburst   ,
-    output wire  [1:0]          arlock    ,
-    output wire  [3:0]          arcache   ,
-    output wire  [2:0]          arprot    ,
+    output wire  [7:0]          arlen     ,     // 0
+    output wire  [2:0]          arsize    ,     // 2
+    output wire  [1:0]          arburst   ,     // 1
+    output wire  [1:0]          arlock    ,     // 0
+    output wire  [3:0]          arcache   ,     // 0
+    output wire  [2:0]          arprot    ,     // 0
     output wire                 arvalid   ,
     input wire                  arready   ,
     // read respond
     input wire  [3:0]           rid       ,
     input wire  [31:0]          rdata     ,
-    input wire  [1:0]           rresp     ,
-    input wire                  rlast     ,
+    input wire  [1:0]           rresp     ,     // ignore
+    input wire                  rlast     ,     // ignore
     input wire                  rvalid    ,
     output wire                 rready    ,
     // write request
@@ -75,6 +75,7 @@ assign arburst =    2'b01;
 assign arlock =     2'b0;
 assign arache =     4'b0;
 assign arprot =     3'b0;
+assign arsize =     3'b010; // 32 bit per time
 
 reg [3:0]       arid_reg;
 reg [31:0]      araddr_reg;
@@ -83,6 +84,7 @@ reg             arvalid_reg;
 
 
 reg             ar_data_addr_valid;
+reg [31:0]      ar_data_addr_reg;
 //  ----------------one hot encoding
 localparam      AR_WAIT =       3'b001,
                 AR_INST_SEND=   3'b010,
@@ -128,14 +130,91 @@ end
 assign inst_sram_addr_ok = ar_current_state == AR_WAIT;
 assign data_sram_addr_ok = ar_current_state == AR_WAIT;
 // --------------------------axi master
+// arid
 assign arid = {2'b0,{ar_current_state == AR_DATA_SEND}};
+// arvalid
+assign arvalid = (ar_current_state == AR_DATA_SEND) || (ar_current_state == AR_INST_SEND);
+// araddr
+always @(posedge clk)begin
+    if(resetn) begin
+        ar_data_addr_reg<= 32'b0;
+        ar_data_addr_valid <= 1'b0;
+    end
+    else if(ar_current_state == AR_WAIT && inst_sram_req && ~inst_sram_wr
+                                        && data_sram_req && ~data_sram_wr )     // Inst fetch and data fetch request at the same time
+    begin
+        ar_data_addr_reg<= data_sram_addr;
+        ar_data_addr_valid <= 1'b1;
+    end
+    else if(ar_current_state == AR_INST_SEND && arready)begin
+        ar_data_addr_reg<= 32'b0;
+        ar_data_addr_valid <= 1'b0; 
+    end
+end
 always @(posedge clk)begin
     if(~resetn)
         araddr_reg <= 32'b0;
-    else if(ar_current_state == AR_WAIT && inst_sram_req & ~inst_sram_wr)
+    else if(ar_current_state == AR_WAIT && inst_sram_req && ~inst_sram_wr)   // Inst fetch
         araddr_reg <= inst_sram_addr;
+    else if(ar_current_state == AR_WAIT && data_sram_req && ~data_sram_wr)
+        araddr_reg <= data_sram_addr;
+    else if(ar_current_state == AR_INST_SEND && arready && ar_data_addr_valid)
+        araddr_reg <= ar_data_addr_reg;
 end
+assign araddr = araddr_reg;
+/*-------------------------------------------------read respond chanel------------------------------------------------------*/
+reg [1:0]                r_current_state;
+reg [1:0]               r_next_state;
 
+reg [31:0]          rdata_reg;
+reg                 rid_reg;
+// ---------------------one hot encoding state
+localparam      R_WAIT =        2'b00,
+                R_RECIEVE =     2'b10;
+
+always @(posedge clk)begin
+    if(~resetn)
+        r_current_state <= R_WAIT;
+    else 
+        r_current_state <= r_next_state;
+end
+// ---------------------next_state generate
+always @(*)begin
+    case(r_current_state)
+        R_WAIT:begin
+            if(rvalid)
+                r_next_state = R_RECIEVE;
+            else
+                r_next_state = R_WAIT;
+        end
+        R_RECIEVE:begin
+            r_next_state = R_WAIT;
+        end
+    endcase
+end
+// --------------------axi master
+assign rready = r_current_state == R_WAIT;
+// --------------------sram slave
+always @(posedge clk)begin
+    if(~resetn)begin
+        rdata_reg <= 32'b0;
+        rid_reg <= 1'b0;
+    end
+    else if(r_current_state == R_WAIT && rvalid)begin
+        rdata_reg <= rdata;
+        rid_reg  <= rid;
+    end
+    else if(r_current_state ==R_RECIEVE)begin
+        rdata_reg <= 32'b0;
+        rid_reg <= 1'b0;
+    end
+end
+// inst_sram
+assign inst_sram_data_ok = (r_current_state == R_RECIEVE && rid_reg == 4'b0);
+assign inst_sram_rdata = rdata_reg;
+// data_sram
+assign data_sram_addr_ok = (r_current_state == R_RECIEVE && rid_reg == 4'b1);
+assign data_sram_rdata = rdata_reg;
 
 
 
